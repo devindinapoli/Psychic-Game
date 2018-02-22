f657c5ce093e6c3d7c52eab26fdc7912df0a8d0e
Devin DiNapoli <devindinapoli@gmail.com>
2018-02-22 17:55:00 -0500

    Modified html, added vars in js

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+var lettersOfAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 9;
+
+var guesses = [];
+var userGuess = null;
+
+var toBeGuessed = lettersOfAlphabet[Math.floor(Math.random() * lettersOfAlphabet.length)];
+
+document.onkeyup = function (event){
+    
+}
+
+   
