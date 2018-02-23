ed1d620498db3f94f2c40777528a861dae935448
Devin DiNapoli <devindinapoli@gmail.com>
2018-02-23 14:13:56 -0500

    Updated js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,8 +9,28 @@ var userGuess = null;
 
 var toBeGuessed = lettersOfAlphabet[Math.floor(Math.random() * lettersOfAlphabet.length)];
 
-document.onkeyup = function (event){
-    
+document.onkeyup = function(event){
+
+var userGuess = event.key.toLowerCase();  
+
+if (guesses.indexOf(userGuess) < 0 && lettersOfAlphabet.indexOf(userGuess) >= 0){
+
+    guesses[guesses.length] = userGuess;
+    guesses--;
+}
+else if (userGuess === toBeGuessed){
+    wins++;
+    guessesLeft = 9;
+    guesses = [];
+    toBeGuessed;
+
 }
 
-   
+else if (guessesLeft === 0){
+    losses++;
+    guessesLeft = 9;
+    guesses: [];
+    toBeGuessed; 
+}
+
+};
